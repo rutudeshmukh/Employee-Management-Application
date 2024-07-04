f061f6adfc3b765a92c428780ad699abc227ace9
Rutuja Deshmukh <rutujad3498@gmail.com>
2024-07-04 18:17:22 +0530

    Initial commit

---
+++ springboot-backend/src/main/java/com/companyz/springboot_backend/model/Employee.java
@@ -0,0 +1,74 @@
+package com.companyz.springboot_backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String emailId;
+
+    @Column(name = "mobile_no")
+    private int mobileNo;
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(long id){
+        this.id =  id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(int mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+    public Employee(){
+
+    }
+
+    public Employee(String firstName,String lastName,String emailId,int mobileNo){
+        super();
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.emailId=emailId;
+        this.mobileNo=mobileNo;
+    }
+}
