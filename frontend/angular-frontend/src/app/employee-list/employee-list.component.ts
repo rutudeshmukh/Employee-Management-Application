5a07edc321ea1094c75a0b379994f6204e27ef63
Rutuja Deshmukh <rutujad3498@gmail.com>
2024-07-06 14:10:00 +0530

    added update employee functionality

--- frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Employee} from '../employee';
 import {EmployeeService} from '../employee.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -11,7 +12,8 @@ export class EmployeeListComponent implements OnInit {
 
   employees: Employee[];
 
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService,
+    private router:Router) { }
 
   ngOnInit(): void {
     this.getEmployees();
@@ -21,5 +23,9 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.getEmployeeList().subscribe(data=>{
       this.employees = data;
     });
+  }
+
+  updateEmployee(id:number){
+    this.router.navigate(['update-employee',id]);
   }
 }
--- springboot-backend/src/main/java/com/companyz/springboot_backend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/companyz/springboot_backend/controller/EmployeeController.java
@@ -37,5 +37,19 @@ public class EmployeeController {
         return ResponseEntity.ok(employee);
     }
 
-    
+    //update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id:"+ id));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+        employee.setMobileNo(employeeDetails.getMobileNo());
+
+        //save updated employee object to the database
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 }
